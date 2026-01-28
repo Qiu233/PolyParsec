a9c228aab7b9ec043693b903ad73dfcd00855eb9
Qiu233 <lopi258@outlook.com>
2026-01-28 14:26:36 +0800

    feat: export symbols

--- PolyParsec.lean
+++ PolyParsec.lean
@@ -1,3 +1,10 @@
 module
 public import PolyParsec.Basic
 public import PolyParsec.Std
+
+namespace PolyParsec
+
+export PolyParsec.MonadPolyParsec (
+  satisfy fail notFollowedBy peek? attempt
+  pchar pstring skipChar skipString optional
+  many many1 manyChars many1Chars)
